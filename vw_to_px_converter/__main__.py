7cd720514d5a77e725f38620ed632f8486732842
Bluesuma <fsggdgjf@gmail.com>
2025-09-01 05:54:37 +0300

    Update README.md

---
+++ vw_to_px_converter/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Entry point for running vw_to_px_converter as a module
+"""
+
+from .converter import main
+
+if __name__ == "__main__":
+    main()
